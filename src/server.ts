f40c83b58edb0b5154203fbe79849e78c599e9ee
Pedro Coelho <pedrohenriquesc@gmail.com>
2022-05-06 11:36:35 -0300

    Ajustes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import { routes } from './routes'
 
 const app = express()
 
+app.use(cors()) // segurança para acesso de outros domínios
 app.use(express.json())
 app.use(routes)
 
